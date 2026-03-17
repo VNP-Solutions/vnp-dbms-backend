4d46ac4445f8bcff075f6d6df8b3f33c9e6187d3
yasirnabil534 <yasir.nabil5343@gmail.com>
2026-03-17 15:57:29 +0600

    Commit-Yasir: Inactive provided in filter fixed

--- src/modules/property/property.service.ts
+++ src/modules/property/property.service.ts
@@ -85,7 +85,7 @@ export class PropertyService implements IPropertyService {
     if (query.booking_status) additionalFilters.booking_status = query.booking_status
     if (query.agoda_id) additionalFilters.agoda_id = query.agoda_id
     if (query.agoda_status) additionalFilters.agoda_status = query.agoda_status
-    if (query.is_active && query.is_active !== 'All') {
+    if (query.is_active !== undefined && query.is_active !== 'All') {
       additionalFilters.is_active = query.is_active
     }
     if (query.start_date && query.end_date) {
