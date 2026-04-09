ce8a60914534cb329c5bbc33738726142089b49d
Yasser Shalash <yasservanshalash@gmail.com>
2026-04-09 14:21:03 +0200

    fix: fixed errors

--- src/modules/activity-log/activity-log.module.ts
+++ src/modules/activity-log/activity-log.module.ts
@@ -7,6 +7,7 @@ import { ActivityLogService } from './activity-log.service'
 @Module({
   controllers: [ActivityLogController],
   providers: [
+    ActivityLogService,
     {
       provide: 'IActivityLogService',
       useClass: ActivityLogService
